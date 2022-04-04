8ff0f1106a8253066bc6d93546f1db38d8f4f664
Alexey D <safrakswork@gmail.com>
2022-04-04 11:16:48 +0300

    added admin-panel settings

--- coolsite/women/apps.py
+++ coolsite/women/apps.py
@@ -1,6 +1,8 @@
+from tabnanny import verbose
 from django.apps import AppConfig
 
 
 class WomenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'women'
+    verbose_name = 'Женщины мира'
